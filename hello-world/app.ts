6dc50bc7120fd1f7dd1fa773dce236f3bc82ff6a
Talwinder Singh <singhtalwinder790@gmail.com>
2023-10-20 22:12:33 +0530

    feat: refactor lambda handler to use product service

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,30 +1,36 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+import { container } from './inversify.config';
+import { IProductService } from './services';
+import { TYPES } from './types';
+import { Product } from './models';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
+  const productService = container.get<IProductService>(TYPES.ProductService);
+
+  try {
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        body: 'Event body is empty',
+      };
     }
+
+    const product: Product = JSON.parse(event.body);
+    await productService.saveProduct(product);
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Product saved',
+      }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Some error happened',
+      }),
+    };
+  }
 };
