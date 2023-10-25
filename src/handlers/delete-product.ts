682b1d92d0052515d1d4887eed79b5e5e293c896
Talwinder Singh <singhtalwinder790@gmail.com>
2023-10-25 16:15:12 +0530

    feat: add delete product API

---
+++ src/handlers/delete-product.ts
@@ -0,0 +1,37 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { container } from '../inversify.config';
+import { IProductService } from '../services';
+import { TYPES } from '../types';
+
+export const deleteProductHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const productService = container.get<IProductService>(TYPES.ProductService);
+
+  try {
+    const productId = event.pathParameters?.id;
+    if (!productId) {
+      return {
+        statusCode: 400,
+        body: 'Product ID is missing',
+      };
+    }
+
+    await productService.deleteProduct(productId);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Product deleted',
+      }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Some error happened',
+      }),
+    };
+  }
+};
+
+
