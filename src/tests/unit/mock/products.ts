abc7ffdadf54986c9685c30eebccfc6ee6b66cb8
Talwinder Singh <singhtalwinder790@gmail.com>
2023-11-04 12:16:06 +0530

    feat: add mock product

---
+++ src/tests/unit/mock/products.ts
@@ -0,0 +1,19 @@
+import { Product } from "../../../models";
+
+export const products: Product[] = [{
+  _id: 'test',
+  category: 'Electronics',
+  images: [
+    'https://example.com/image1.jpg',
+    'https://example.com/image2.jpg'
+  ],
+  price: 99.99,
+  description: 'This is an example product description.',
+  name: 'Example Product',
+  sku: 'SKU12345',
+  tags: [
+    'tag1',
+    'tag2',
+    'tag3'
+  ]
+}];
