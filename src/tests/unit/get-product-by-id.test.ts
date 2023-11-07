be548e7c69f6759363cf4c9b42cf4c886e8d1b99
Talwinder Singh <singhtalwinder790@gmail.com>
2023-11-07 22:21:09 +0530

    feat: add get product by id tests

---
+++ src/tests/unit/get-product-by-id.test.ts
@@ -0,0 +1,32 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { expect, describe, it } from '@jest/globals';
+import { event as baseEvent } from './mock/event';
+import { getProductByIdHandler } from '../../handlers/get-product-by-id';
+import { mockProductService } from './mock/mock-product.service';
+import { container } from '../../inversify.config';
+import { IProductService } from '../../services';
+import { products } from './mock/products';
+import { TYPES } from '../../types';
+
+container.rebind<IProductService>(TYPES.ProductService).toConstantValue(mockProductService);
+
+describe('getProductByIdHandler', function () {
+
+  describe('when correct data is passed', function () {
+    it('should return successful response', async () => {
+      const event: APIGatewayProxyEvent = {
+        ...baseEvent,
+        httpMethod: 'GET',
+        path: '/product/test',
+        pathParameters: {
+          id: 'test'
+        },
+      };
+  
+      const result: APIGatewayProxyResult = await getProductByIdHandler(event);
+  
+      expect(result.statusCode).toEqual(200);
+      expect(result.body).toEqual(JSON.stringify(products[0]));
+    });
+  });
+});
